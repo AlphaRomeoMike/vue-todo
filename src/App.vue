15f3638400c27dbcc4a13c2c5f74cc7ef7d148b7
unknown <AlphaRomeoMike>
2021-08-11 19:02:37 +0500

    completed the todo app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,147 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <Header 
+      @toggle-add-task="toggleAddTask" 
+    title="Task Tracker" />
+    
+    <div v-if="showAddTask">
+      <AddTask 
+        @add-task="addTask" />
+    </div>
+    <Tasks 
+      @toggle-reminder="toggleReminder" 
+      @delete-task="deleteTask" 
+      :tasks="tasks"/>
+    <Footer />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Header from './components/Header.vue';
+import Tasks from './components/Tasks.vue'
+import AddTask from './components/AddTask.vue'
+import Footer from './components/Footer.vue'
 
 export default {
-  name: "App",
+  name: 'Todo App',
   components: {
-    HelloWorld,
+    Header,
+    Tasks,
+    AddTask,
+    Footer
   },
+  data() {
+    return {
+      tasks: [],
+      showAddTask: false
+    }
+  },
+  methods: {
+    toggleAddTask(){
+      this.showAddTask = !this.showAddTask;
+    },
+    async addTask(task){
+      const res = await fetch('api/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(task)
+      })
+
+      const data = await res.json();
+
+      this.tasks = [...this.tasks, data ]
+    },
+    async deleteTask(id) {
+
+      if(confirm('Are you sure you dum dum?')) {
+        const res = await fetch(`api/tasks/${id}`, {
+          method: 'DELETE',
+        })
+
+        res.status === 200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert('Could not delete')
+      }
+    },
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id)
+
+      const updateTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+      const res = await fetch(`api/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updateTask)
+      })
+
+      const data = await res.json()
+
+      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: data.reminder} : task)
+    },
+    async fetchTasks(){
+      const res = await fetch('api/tasks')
+
+      const data = await res.json()
+
+      return data;
+    },
+    async fetchTask(id){
+      const res = await fetch(`api/tasks/${id}`)
+
+      const data = await res.json()
+
+      return data;
+    }  
+  },
+
+  async created() {  
+    this.tasks = await this.fetchTasks();
+  }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button @toggle-add-task="$emit('toggle-add-task')" @click="logData()" text="Add task" color="black"/>
+    </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+    export default {
+        name: 'Header',
+        props: {
+            title: {
+                type: String,
+                }
+        },
+        components: {
+            Button
+        },
+        methods: {
+            logData() {
+                console.log('Hello, dumbass!');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+    }    
+</style>
